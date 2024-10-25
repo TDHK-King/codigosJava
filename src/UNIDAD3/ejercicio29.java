cebba945c59c6a2bd38c3a58878056ce8291407b
TDHK-King <max.walter.velez@epet14.edu.ar>
2024-10-25 20:02:53 -0300

    se termina ejercicio29

--- src/UNIDAD3/ejercicio29.java
+++ src/UNIDAD3/ejercicio29.java
@@ -1,12 +1,14 @@
 package UNIDAD3;
 
+import java.io.IOException;
+
 import static UNIDAD3.ejercicio19.imprimirMatriz;
 import static UNIDAD3.ejercicio20.matrizAleatoria;
 
 public class ejercicio29 {
     public static void main(String[] args) {
     int[][] matriz1 = matrizAleatoria(3,2,0,10);
-    int[][] matriz2 = matrizAleatoria(2,3,0,10);
+    int[][] matriz2 = matrizAleatoria(3,4,0,10);
         imprimirMatriz(matriz1);
         System.out.println("la segunda matriz es: ");
         imprimirMatriz(matriz2);
@@ -17,22 +19,27 @@ public class ejercicio29 {
 
 
     public static int[][] productoDeMatrices(int[][] matriz1,int[][] matriz2){
-        int[][] matrizresultante = new int[matriz1[0].length][matriz2.length];
-        for (int i = 0;i < matrizresultante.length;i++){
-            for (int j = 0;j < matrizresultante[i].length;j++){
+        if (matriz1[0].length != matriz2.length){
+            throw new IllegalArgumentException("las filas de la primer matriz deben tener el mismo valor que las columnas de la 2da");
+        }
+        int[][] matrizResultante = new int[matriz1.length][matriz2[0].length];
 
+        for (int i = 0;i < matriz1.length;i++) {
+            for (int j = 0; j < matriz2[0].length; j++) {
+                int suma = 0;
+                for (int k = 0; k < matriz1[0].length; k++) {
+                    suma += Math.abs(matriz1[i][k]) * Math.abs(matriz2[k][j]);
 
-
+                }
+                matrizResultante[i][j] = suma;
             }
-
-
         }
 
 
 
 
 
-    return matrizresultante;
+        return matrizResultante;
     }
 
 
