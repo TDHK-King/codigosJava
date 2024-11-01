01c9164af691586598a6e68123ff0ddbaa95ee29
TDHK-King <max.walter.velez@epet14.edu.ar>
2024-11-01 03:05:28 -0300

    se añaden ejercicios hasta el 29

---
+++ src/UNIDAD3/ejercicioClaseConsulta.java
@@ -0,0 +1,31 @@
+package UNIDAD3;
+
+import static UNIDAD3.ejercicio19.*;
+import static UNIDAD3.ejercicio20.*;
+
+
+public class ejercicioClaseConsulta {
+    public static void main(String[] args) {
+        int[][] matriz = matrizAleatoria(4,3,0,10);
+        int[] cords = new int[2];
+
+    }
+
+    public static void floodFill(int[][] matriz,int[] cords,int contagio){
+        
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
